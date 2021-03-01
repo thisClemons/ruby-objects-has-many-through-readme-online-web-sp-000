80c43720310c8ad3106a45a155d14921ac8a7a16
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-01 23:14:22 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -21,4 +21,12 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end
 
+  def meals
+    Meal.all.select {|m| m.customer == self}
+  end
+
+  def waiters
+    Waiter.all.select {|w| w.customer == self}
+  end
+
 end
