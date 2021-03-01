8fe6697c2f1506610a26ce8ef90e5ad5e54c8ca6
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-01 23:13:01 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,16 +1,25 @@
 class Meal
 
+  attr_reader :customer
+  
   @@all = []
+
 
   def initialize(waiter, customer, total, tip)
     @waiter = waiter
     @customer = customer
     @total = total
     @tip = tip
+
+    save
+  end
+
+  def save
+    @@all << self
   end
 
   def self.all
-    @@all 
+    @@all
   end
 
 end
