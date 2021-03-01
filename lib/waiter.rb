8c2101744901dac5e34621700a64187c60430a18
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-01 23:14:11 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,22 @@
 class Waiter
 
+  attr_reader :customer
+
+  @@all = []
+
+  def initialize(name, years)
+    @name = name
+    @years = years
+
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
